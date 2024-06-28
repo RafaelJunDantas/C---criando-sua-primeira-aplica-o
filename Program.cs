1215200e8cf11a411f57a0d143b9867129fadda3
jun <rafajdantas@gmail.com>
2024-06-28 08:14:13 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿// Screen Sound
+string mensagemBoasVindas = "Boas vindas ao Screen Sound";
+
+void PrintMensagem()
+{
+    Console.WriteLine(mensagemBoasVindas);    
+}
+
+void PrintMenu()
+{
+    Console.WriteLine("\nDigite 1 para registrar uma banda");
+    Console.WriteLine("Digite 2 para mostrar todas as bandas");
+    Console.WriteLine("Digite 3 para avaliar uma banda");
+    Console.WriteLine("Digite 4 para exibir a média");
+    Console.WriteLine("Digite -1 para sair\n");
+
+    Console.Write("Digite uma opção: ");
+    int opcao = int.Parse(Console.ReadLine()!);
+
+    Console.WriteLine("Opção digitada: " + opcao);
+}
+
+PrintMensagem();
+PrintMenu();
---
+++ README.md
@@ -0,0 +1,4 @@
+# Repositório para o curso C#: criando sua primeira aplicação
+
+- `dotnet new console`
+- `dotnet run nome.cs` 
